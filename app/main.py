9b27473c85ee33c91bb69cb01a31995e34b52c04
Saliou DIEDHIOU <133891956+SDKisito@users.noreply.github.com>
2025-05-21 11:51:50 +0200

    main.py

--- app/main.py
+++ app/main.py
@@ -5,6 +5,4 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"message": "Application Examen Jenkins pour Saliou DIEDHIOU"}
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=80)
+
